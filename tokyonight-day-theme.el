88cfe592e09a1f4832134067411ef923d19ccb54
xuchengpeng <chuckxcp@qq.com>
2026-08-19 16:45:26 +0800

    build on top of modus-themes

--- tokyonight-day-theme.el
+++ tokyonight-day-theme.el
@@ -2,62 +2,77 @@
 ;;; Commentary:
 ;;; Code:
 
-(eval-and-compile
-  (unless (and (fboundp 'require-theme)
-               load-file-name
-               (equal (file-name-directory load-file-name)
-                      (expand-file-name "themes/" data-directory))
-               (require-theme 'tokyonight-themes t))
-    (require 'tokyonight-themes))
+(require 'tokyonight-themes)
 
-  ;;;###theme-autoload
-  (deftheme tokyonight-day
-    "TokyoNight Night."
-    :background-mode 'light
-    :kind 'color-scheme
-    :family 'tokyonight)
+(defconst tokyonight-day-base-colors
+  '((bg "#e1e2e7")
+    (bg-dark "#d0d5e3")
+    (bg-dark1 "#c1c9df")
+    (bg-hl "#c4c8da")
+    (blue0 "#7890dd")
+    (blue "#2e7de9")
+    (blue1 "#188092")
+    (blue2 "#07879d")
+    (blue5 "#006a83")
+    (blue6 "#2e5857")
+    (blue7 "#92a6d5")
+    (comment "#848cb5")
+    (cyan "#007197")
+    (dark3 "#8990b3")
+    (dark5 "#68709a")
+    (fg "#3760bf")
+    (fg-dark "#6172b0")
+    (fg-gutter "#a8aecb")
+    (green "#587539")
+    (green1 "#387068")
+    (green2 "#38919f")
+    (magenta "#9854f1")
+    (magenta2 "#d20065")
+    (orange "#b15c00")
+    (purple "#7847bd")
+    (red "#f52a65")
+    (red1 "#c64343")
+    (teal "#118c74")
+    (terminal-black "#a1a6c5")
+    (yellow "#8c6c3e"))
+  "Tokyonight day base colors.")
 
-  (defconst tokyonight-day-palette
-    '((bg . "#e1e2e7")
-      (bg-dark . "#d0d5e3")
-      (bg-dark1 . "#c1c9df")
-      (bg-hl . "#c4c8da")
-      (blue0 . "#7890dd")
-      (blue . "#2e7de9")
-      (blue1 . "#188092")
-      (blue2 . "#07879d")
-      (blue5 . "#006a83")
-      (blue6 . "#2e5857")
-      (blue7 . "#92a6d5")
-      (comment . "#848cb5")
-      (cyan . "#007197")
-      (dark3 . "#8990b3")
-      (dark5 . "#68709a")
-      (fg . "#3760bf")
-      (fg-dark . "#6172b0")
-      (fg-gutter . "#a8aecb")
-      (green . "#587539")
-      (green1 . "#387068")
-      (green2 . "#38919f")
-      (magenta . "#9854f1")
-      (magenta2 . "#d20065")
-      (orange . "#b15c00")
-      (purple . "#7847bd")
-      (red . "#f52a65")
-      (red1 . "#c64343")
-      (teal . "#118c74")
-      (terminal-black . "#a1a6c5")
-      (yellow . "#8c6c3e"))
-    "TokyoNight day palette.")
+(defconst tokyonight-day-palette
+  (append
+   (let* ((base-colors tokyonight-day-base-colors)
+          (bg-main (car (alist-get 'bg base-colors)))
+          (six-colors (seq-filter
+                       (lambda (color)
+                         (memq (car color) '(red green yellow blue purple cyan)))
+                       base-colors))
+          (derived-colors nil))
+     (pcase-dolist (`(,name ,value) six-colors)
+       (push (list (intern (format "%s-warmer" name)) (tokyonight-themes-darken (tokyonight-themes-blend value "#ff0000" 0.9) 20)) derived-colors)
+       (push (list (intern (format "%s-cooler" name)) (tokyonight-themes-darken (tokyonight-themes-blend value "#0000ff" 0.9) 20)) derived-colors)
+       (push (list (intern (format "%s-faint" name)) (tokyonight-themes-darken value 10)) derived-colors)
+       (push (list (intern (format "%s-intense" name)) (tokyonight-themes-lighten value 5)) derived-colors)
+       (push (list (intern (format "bg-%s-intense" name)) (tokyonight-themes-lighten value 35)) derived-colors)
+       (push (list (intern (format "bg-%s-subtle" name)) (tokyonight-themes-lighten value 55))  derived-colors)
+       (push (list (intern (format "bg-%s-nuanced" name)) (tokyonight-themes-lighten value 75)) derived-colors))
+     (push (list (intern "highlight") (tokyonight-themes-darken bg-main 5)) derived-colors)
+     (append
+      base-colors
+      derived-colors))
+   tokyonight-themes-common-palette-mappings)
+  "Tokyonight day palette.")
 
-  (defcustom tokyonight-day-palette-overrides nil
-    "Overrides for `tokyonight-day-palette'."
-    :group 'tokyonight-themes
-    :type 'alist)
+(defcustom tokyonight-day-palette-overrides nil
+  "Overrides for `tokyonight-day-palette'."
+  :group 'tokyonight-themes
+  :type 'alist)
 
-  (tokyonight-themes-theme tokyonight-day
-                           tokyonight-day-palette
-                           tokyonight-day-palette-overrides)
+(modus-themes-theme
+ 'tokyonight-day
+ 'tokyonight-themes
+ "Tokyonight day."
+ 'light
+ 'modus-operandi-palette
+ 'tokyonight-day-palette
+ 'tokyonight-day-palette-overrides)
 
-  (provide-theme 'tokyonight-day))
 ;;; tokyonight-day-theme.el ends here
--- tokyonight-moon-theme.el
+++ tokyonight-moon-theme.el
@@ -2,62 +2,77 @@
 ;;; Commentary:
 ;;; Code:
 
-(eval-and-compile
-  (unless (and (fboundp 'require-theme)
-               load-file-name
-               (equal (file-name-directory load-file-name)
-                      (expand-file-name "themes/" data-directory))
-               (require-theme 'tokyonight-themes t))
-    (require 'tokyonight-themes))
+(require 'tokyonight-themes)
 
-  ;;;###theme-autoload
-  (deftheme tokyonight-moon
-    "TokyoNight Night."
-    :background-mode 'dark
-    :kind 'color-scheme
-    :family 'tokyonight)
+(defconst tokyonight-moon-base-colors
+  '((bg "#222436")
+    (bg-dark "#1e2030")
+    (bg-dark1 "#191b29")
+    (bg-hl "#2f334d")
+    (blue0 "#3e68d7")
+    (blue "#82aaff")
+    (blue1 "#65bcff")
+    (blue2 "#0db9d7")
+    (blue5 "#89ddff")
+    (blue6 "#b4f9f8")
+    (blue7 "#394b70")
+    (comment "#7a88cf")
+    (cyan "#86e1fc")
+    (dark3 "#545c7e")
+    (dark5 "#737aa2")
+    (fg "#c8d3f5")
+    (fg-dark "#828bb8")
+    (fg-gutter "#3b4261")
+    (green "#c3e88d")
+    (green1 "#4fd6be")
+    (green2 "#41a6b5")
+    (magenta "#c099ff")
+    (magenta2 "#ff007c")
+    (orange "#ff966c")
+    (purple "#fca7ea")
+    (red "#ff757f")
+    (red1 "#c53b53")
+    (teal "#4fd6be")
+    (terminal-black "#444a73")
+    (yellow "#ffc777"))
+  "Tokyonight moon base colors.")
 
-  (defconst tokyonight-moon-palette
-    '((bg . "#222436")
-      (bg-dark . "#1e2030")
-      (bg-dark1 . "#191b29")
-      (bg-hl . "#2f334d")
-      (blue0 . "#3e68d7")
-      (blue . "#82aaff")
-      (blue1 . "#65bcff")
-      (blue2 . "#0db9d7")
-      (blue5 . "#89ddff")
-      (blue6 . "#b4f9f8")
-      (blue7 . "#394b70")
-      (comment . "#7a88cf")
-      (cyan . "#86e1fc")
-      (dark3 . "#545c7e")
-      (dark5 . "#737aa2")
-      (fg . "#c8d3f5")
-      (fg-dark . "#828bb8")
-      (fg-gutter . "#3b4261")
-      (green . "#c3e88d")
-      (green1 . "#4fd6be")
-      (green2 . "#41a6b5")
-      (magenta . "#c099ff")
-      (magenta2 . "#ff007c")
-      (orange . "#ff966c")
-      (purple . "#fca7ea")
-      (red . "#ff757f")
-      (red1 . "#c53b53")
-      (teal . "#4fd6be")
-      (terminal-black . "#444a73")
-      (yellow . "#ffc777"))
-    "TokyoNight moon palette.")
+(defconst tokyonight-moon-palette
+  (append
+   (let* ((base-colors tokyonight-moon-base-colors)
+          (bg-main (car (alist-get 'bg base-colors)))
+          (six-colors (seq-filter
+                       (lambda (color)
+                         (memq (car color) '(red green yellow blue purple cyan)))
+                       base-colors))
+          (derived-colors nil))
+     (pcase-dolist (`(,name ,value) six-colors)
+       (push (list (intern (format "%s-warmer" name)) (tokyonight-themes-lighten (tokyonight-themes-blend value "#ff0000" 0.9) 20)) derived-colors)
+       (push (list (intern (format "%s-cooler" name)) (tokyonight-themes-lighten (tokyonight-themes-blend value "#0000ff" 0.9) 20)) derived-colors)
+       (push (list (intern (format "%s-faint" name)) (tokyonight-themes-lighten value 10)) derived-colors)
+       (push (list (intern (format "%s-intense" name)) (tokyonight-themes-darken value 5)) derived-colors)
+       (push (list (intern (format "bg-%s-intense" name)) (tokyonight-themes-darken value 35)) derived-colors)
+       (push (list (intern (format "bg-%s-subtle" name)) (tokyonight-themes-darken value 55))  derived-colors)
+       (push (list (intern (format "bg-%s-nuanced" name)) (tokyonight-themes-darken value 75)) derived-colors))
+     (push (list (intern "highlight") (tokyonight-themes-lighten bg-main 5)) derived-colors)
+     (append
+      base-colors
+      derived-colors))
+   tokyonight-themes-common-palette-mappings)
+  "Tokyonight moon palette.")
 
-  (defcustom tokyonight-moon-palette-overrides nil
-    "Overrides for `tokyonight-moon-palette'."
-    :group 'tokyonight-themes
-    :type 'alist)
+(defcustom tokyonight-moon-palette-overrides nil
+  "Overrides for `tokyonight-moon-palette'."
+  :group 'tokyonight-themes
+  :type 'alist)
 
-  (tokyonight-themes-theme tokyonight-moon
-                           tokyonight-moon-palette
-                           tokyonight-moon-palette-overrides)
+(modus-themes-theme
+ 'tokyonight-moon
+ 'tokyonight-themes
+ "Tokyonight moon."
+ 'dark
+ 'modus-vivendi-palette
+ 'tokyonight-moon-palette
+ 'tokyonight-moon-palette-overrides)
 
-  (provide-theme 'tokyonight-moon))
 ;;; tokyonight-moon-theme.el ends here
