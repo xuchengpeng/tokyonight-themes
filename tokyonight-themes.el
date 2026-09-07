b8622211a7f1f2a425a97901ce4cf4e766890b3a
xuchengpeng <chuckxcp@qq.com>
2026-09-07 08:58:51 +0800

    update region

--- tokyonight-themes.el
+++ tokyonight-themes.el
@@ -61,7 +61,7 @@
               `(link-visited ((t :background ,bg :foreground ,purple :underline ,purple)))
               `(shadow ((t :foreground ,fg-dark)))
               `(minibuffer-prompt ((t :foreground ,cyan :background unspecified)))
-              `(region ((t :background ,terminal-black :foreground ,fg :extend t)))
+              `(region ((t :background ,dark3 :foreground ,fg :extend t)))
               `(secondary-selection ((t :background ,bg-hl :foreground unspecified)))
               `(trailing-whitespace ((t :foreground ,fg :background ,red1)))
               `(border ((t :background ,dark3 :foreground ,fg)))
